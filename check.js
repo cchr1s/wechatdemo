c914174bf2683cbb962b07dc200ef1dfe687ab8f
clippers <798491055@qq.com>
2019-10-21 22:31:55 +0800

    first init

---
+++ check.js
@@ -0,0 +1,39 @@
+const Koa =  require('koa')
+const sha1 = require('sha1')
+
+const config = {
+    wechat: {
+        appID: 'wxfb4cb79b59b19ef9',
+        appsecret: 'aed0886151622b3c867dc5c45a8f07c6',
+        token: 'clipperswaschampionthisseason',
+    }
+}
+
+const app = new Koa()
+
+// 加载认证中间件
+// ctx 是 Koa 的应用上下文
+// next 是串联中间件的钩子函数
+app.use(async (ctx, next) => {
+    console.log(ctx.query)
+    const {
+        signature,
+        timestamp,
+        nonce,
+        echostr
+    } = ctx.query
+
+    const token = config.wechat.token
+    let str = [token, timestamp, nonce].sort().join('')
+
+    const sha = sha1(str)
+
+    if (sha === signature) {
+        ctx.body = echostr
+    } else { 
+        ctx.body = 'wrong'
+    }
+})
+
+app.listen(3007)
+console.log(`listen on : 3007`)
